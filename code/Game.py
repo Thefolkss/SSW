f12bdeb16453386945b28329e7c28366d196d79b
TheFolkss <felipeknaut@hotmail.com>
2025-03-28 21:34:06 -0300

    Creating image and const.py Connecting const

--- code/Game.py
+++ code/Game.py
@@ -4,12 +4,13 @@
 import pygame
 
 from code.Menu import Menu
+from const import WIN_WIDTH, WIN_HEIGHT
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(800, 600))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH,WIN_HEIGHT))
 
     def run(self):
         while True:
