e200c098d7bb34ffd64f0eceae29275b2ff33bbc
TheFolkss <felipeknaut@hotmail.com>
2025-03-30 11:37:41 -0300

    Fixing imports

--- code/Menu.py
+++ code/Menu.py
@@ -10,7 +10,7 @@ from code.Const import WIN_WIDTH, COLOR_WHITE, MENU_OPTION, COLOR_GRAY
 class Menu:
     def __init__(self, window):
         self.window = window
-        self.surf = pygame.image.load('./asset/Clouds3.png')
+        self.surf = pygame.image.load('./asset/Clouds3.png').convert_alpha()
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
