75593d69242f4333003bfb57c637606dc9fe0aa5
TheFolkss <felipeknaut@hotmail.com>
2025-03-30 14:19:13 -0300

    Creating players and enemies Moving players and enemies

--- code/Const.py
+++ code/Const.py
@@ -1,23 +1,43 @@
+import pygame
+
+# C
+COLOR_WHITE = (255, 255, 255)
+COLOR_GRAY = (128, 128, 128)
+COLOR_GREEN = (161, 251, 142)
+
+# E
+EVENT_ENEMY = pygame.USEREVENT + 1
+ENTITY_SPEED = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 1,
+    'Level1Bg2': 2,
+    'Level1Bg3': 3,
+    'Player1': 3,
+    'Player2': 3,
+    'Enemy1': 2,
+    'Enemy2': 1,
+}
+
+# M
+MENU_OPTION = ('NOVO JOGO - 1P',
+               'NOVO JOGO - 2P COOPERATIVO',
+               'NOVO JOGO- 2P COMPETITIVO',
+               'PLACAR',
+               'SAIR')
+# P
+PLAYER_KEY_UP = {'Player1': pygame.K_w,
+                 'Player2': pygame.K_UP}
+PLAYER_KEY_DOWN = {'Player1': pygame.K_s,
+                   'Player2': pygame.K_DOWN}
+PLAYER_KEY_LEFT = {'Player1': pygame.K_a,
+                   'Player2': pygame.K_LEFT}
+PLAYER_KEY_RIGHT = {'Player1': pygame.K_d,
+                    'Player2': pygame.K_RIGHT}
+PLAYER_KEY_SHOOT = {'Player1': pygame.K_SPACE,
+                    'Player2': pygame.K_0}
+#S
+SPAWN_TIME = 4000
+
 # W
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
-
-# C
-COLOR_WHITE= (255, 255, 255)
-COLOR_GRAY= (128, 128, 128)
-COLOR_GREEN= (161, 251, 142)
-
-# M
-MENU_OPTION= ('NOVO JOGO - 1P',
-              'NOVO JOGO - 2P COOPERATIVO',
-              'NOVO JOGO- 2P COMPETITIVO',
-              'PLACAR',
-              'SAIR')
-
-# E
-ENTITY_SPEED = {
-    'Level1Bg0':0,
-    'Level1Bg1':1,
-    'Level1Bg2':2,
-    'Level1Bg3':3,
-}
--- code/Level.py
+++ code/Level.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import random
 import sys
 
 import pygame
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import WIN_HEIGHT, COLOR_WHITE
+from code.Const import WIN_HEIGHT, COLOR_WHITE, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME
 from code.Entity import Entity
 from code.EntityFactory import EntityFactory
 
 
 class Level:
     def __init__(self, window, name, game_mode):
+        self.timeout = 30000  # 20 segundos
         self.window = window
         self.name = name
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
-        self.timeout = 30000  # 20 segundos
+        self.entity_list.append(EntityFactory.get_entity('Player1'))  # Size 50x75
+        if game_mode in [MENU_OPTION[1], MENU_OPTION[2]]:
+            self.entity_list.append(EntityFactory.get_entity('Player2'))  # Size 50x75
+        pygame.time.set_timer(EVENT_ENEMY, SPAWN_TIME)
 
     def run(self):
         pygame.mixer_music.load(f'./asset/Level1Sound.wav')
@@ -33,13 +38,16 @@ class Level:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type ==EVENT_ENEMY:
+                    choice = random.choice(('Enemy1', 'Enemy2'))
+                    self.entity_list.append(EntityFactory.get_entity(choice))
 
             # Printed text
-            self.level_text(16, f'{self.name} - Tempo: {self.timeout / 1000 :.1f}s', COLOR_WHITE, (10, 5))
-            self.level_text(16, f'fps: {clock.get_fps() :.0f}', COLOR_WHITE, (10, WIN_HEIGHT - 35))
-            self.level_text(16, f'entidades: {len(self.entity_list)}', COLOR_WHITE, (10, WIN_HEIGHT - 20))
+            self.level_text(16, f'Fase 1 - Tempo: {self.timeout / 1000 :.1f}s', COLOR_WHITE, (10, 5))
+            self.level_text(16, f'fps: {clock.get_fps() :.0f}', COLOR_WHITE, (10, WIN_HEIGHT - 15))
+            # self.level_text(16, f'entidades: {len(self.entity_list)}', COLOR_WHITE, (10, WIN_HEIGHT - 20))
             pygame.display.flip()
-        #pass
+        # pass
 
     def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
